ae0f98dbe25a1f69ca7c4decb1e9bf355b2c3b7b
Çınar Özgürgil <cozgurgil@gmail.com>
2025-07-08 15:33:11 +0300

    Bölüm8-Finish

---
+++ php-eğitim/index.php
@@ -0,0 +1,9 @@
+<!-- <?php echo "Php Eğitiminin ilk aşaması" ?> -->
+
+<!-- Noktalı virgül ile aşşağıdaki gibi bir satır aşşağıya inip yazmaya devam edebiliriz. -->
+
+<?php echo "Php Eğitimi"; 
+echo "Devam";
+
+
+
