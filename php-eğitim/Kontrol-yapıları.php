97ea415a7a40d0849a251a818024d09033289fdb
Çınar Özgürgil <cozgurgil@gmail.com>
2025-07-13 14:11:08 +0300

    Döngüler

--- php-eğitim/Kontrol-yapıları.php
+++ php-eğitim/Kontrol-yapıları.php
@@ -29,3 +29,38 @@ switch ($gun) {
 }
 
 ?>
+
+<?php 
+
+/* Döngüler */
+
+
+// Sadece PHP de foreach içerisine girilen değerin adı farklı yani as değeri
+
+$dizi = ["PHP","Javascript","Python"];
+
+foreach($dizi as $deger) { // Burada girdiğimiz (as) değeri PHP ye özeldir.
+    echo $deger . " ";
+}
+
+// Foreach dizisinde anahtar değer ilişkisi.
+
+$kategoriler = [
+    1 => "Elektronik",
+    2 => "Ev",
+    3 => "Yaşam",
+    4 => "Kitap",
+    5 => "Moda",
+];
+
+echo "<h3>Kategoriler</h3>";
+echo "<ul>";
+
+foreach($kategoriler as $id => $kategori) { // Burada kategoriler içerisinde bir eşleşme yani bir ID ,değer ilişkisi olduğu için böyle yaparız.
+    // Burada $id değeri sayılara , karşısındaki değerler de $kategori değerine eşit olur böylelikle iki değeri de aynı foreach içerisinde döndürebiliriz.
+    echo "<li>Kategori ID: {$id} , Adı: {$kategori}</li>";
+}
+
+echo "</ul>";
+
+?>
