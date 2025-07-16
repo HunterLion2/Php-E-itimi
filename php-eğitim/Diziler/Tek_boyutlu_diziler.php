db00fdc5854d74c5093220ea202ca958e5de00e0
Çınar Özgürgil <cozgurgil@gmail.com>
2025-07-16 14:05:35 +0300

    Tek-boyutlu-diziler

---
+++ php-eğitim/Diziler/Tek_boyutlu_diziler.php
@@ -0,0 +1,26 @@
+<?php
+
+/* Tek Boyutlu Diziler */
+
+// Tek boyutlu diziler, tek bir düzeyde veri içerir. Bu dizilerde veriler indeksler yardımıyla erişilebilir.
+
+// Sayısal İndeksli Bir Dizi
+
+$meyveler = ["Elma","Armut","Muz"];
+
+foreach($meyveler as $meyve) {
+    echo $meyve . "<br>";
+}
+
+
+/* Anahtar Değer İlişkili Dizi */
+
+$kisi = [
+    "isim" => "Ahmet",
+    "yas" => 25,
+    "sehir" => "İstanbul"
+];
+
+echo $kisi['isim']; // Bu şekilde anahtarını yazarız değeri gelir.
+
+?>
